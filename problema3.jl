9af44dd7820cc2326f5e6b77a56c81026987401b
ppguz <ppguzmanmayen@gmail.com>
2025-08-04 23:42:04 -0600

    Feat: ejercicio 2

--- problema3.jl
+++ problema3.jl
@@ -1,61 +1,44 @@
-using JuMP, HiGHS, Printf
+using JuMP
+using HiGHS
 
-# a) Formular el problema de programamci´on lineal.
+# Matriz de costos (usamos 9999 como valor prohibido)
+INF = 9999
+cost = [
+    50   50    INF  20;
+    70   40    20   30;
+    90   30    50   INF;
+    70   20    60   70
+]
 
-# Sabemos que tenemos 6 horarios y el $$x_i$$ representa los buses entonces la funcion a minimizar sera la suma de los buses
+n_trabajadores, n_puestos = size(cost)
 
+model = Model(HiGHS.Optimizer)
 
-#$$F(z) = x_1 + x_2 + x_3 + x_4 + x_5 + x_6$$ 
+@variable(model, x[1:n_trabajadores, 1:n_puestos], Bin)
 
-# Esto sera la funcion objetivo a minimizar ya que es la suma de los buses y las restricciones son los horarios
+# Función objetivo: minimizar el costo total
+@objective(model, Min, sum(cost[i,j] * x[i,j] for i in 1:n_trabajadores, j in 1:n_puestos))
 
-# 12:00-4:00 AM: x₁ + x₆ ≥ 4
-# 4:00-8:00 AM: x₁ + x₂ ≥ 8 
-# 8:00 AM-12:00 PM: x₂ + x₃ ≥ 10
-# 12:00-4:00 PM: x₃ + x₄ ≥ 7 
-# 4:00-8:00 PM: x₄ + x₅ ≥ 12 
-# 8:00 PM-12:00 AM: x₅ + x₆ ≥ 4
+# Restricción: cada trabajador solo en un puesto
+@constraint(model, [i=1:n_trabajadores], sum(x[i,j] for j in 1:n_puestos) == 1)
 
-# Esto conforme a lo que se presento en la grafica ya que la suma de los buses de los turnos tiene que cumplir minimo con el requisito de la demanda, y la demanda es el numero despues del ≥
-# La restriccion de no negatividad es que siempre existan buses
+# Restricción: cada puesto solo asignado a un trabajador
+@constraint(model, [j=1:n_puestos], sum(x[i,j] for i in 1:n_trabajadores) == 1)
 
-# Ahora solo lo convertimos a un problema de programacon con lua
-
-model = Model()
-
-
-@variable(model, x1 >= 0)
-@variable(model, x2 >= 0)
-@variable(model, x3 >= 0)
-@variable(model, x4 >= 0)
-@variable(model, x5 >= 0)
-@variable(model, x6 >= 0)
-
-@constraint(model, x1 + x6 >= 4)
-@constraint(model, x1 + x2 >= 8)
-@constraint(model, x2 + x3 >= 10)
-@constraint(model, x3 + x4 >= 7)
-@constraint(model, x4 + x5 >= 12)
-@constraint(model, x5 + x6 >= 4)
-
-@objective(model, Min, x1 + x2 + x3 + x4 + x5 + x6 )
-
-set_optimizer(model, HiGHS.Optimizer)
-
+# Eliminar asignaciones prohibidas (INF)
+for i in 1:n_trabajadores, j in 1:n_puestos
+    if cost[i,j] == INF
+        fix(x[i,j], 0.0; force=true)
+    end
+end
 
 optimize!(model)
 
+println("Resultado:")
+for i in 1:n_trabajadores, j in 1:n_puestos
+    if value(x[i,j]) > 0.5
+        println("  Trabajador $i → Puesto $j (Costo: \$$(cost[i,j]))")
+    end
+end
 
-
-
-print("x1 = ", value.(x1), "\n")
-print("x2 = ", value.(x2), "\n")
-print("x3 = ", value.(x3), "\n")
-print("x4 = ", value.(x4), "\n\n")
-print("x5 = ", value.(x5), "\n\n")
-print("x6 = ", value.(x6), "\n\n")
-
-print("z = ", objective_value(model))
-
-
-# Por lo que el total minimo de buses para que todo se cumpla con 26
+println("Costo total mínimo: \$", objective_value(model))
