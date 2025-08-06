f1bcb3ee0bfecb85d59295bb1133fd37c3cdf8ff
ppguz <ppguzmanmayen@gmail.com>
2025-08-06 13:08:43 -0600

    Feat: secante

--- problema4.py
+++ problema4.py
@@ -5,15 +5,17 @@ def biseccion(a, b, funcion, iter, tol):
     
     if funcion(a)*funcion(b) >= 0:
         print("Error, en el intervalo inicial debe haber un cambio de signo")
-        return None
+        return None, [], []
+    aproximaciones = []
+    yaproximaciones = []
     
-
-    for i in range(iter):
+    for i in range(iter+1):
         x = (a+b)/2
-
+        aproximaciones.append(x)
+        yaproximaciones.append(funcion(x))
         if funcion(x) == 0 or np.abs(a-b) < tol:
             print(f"Iteración {i+1}: raíz aproximada encontrada en x = {x}")
-            return x
+            return x, aproximaciones, yaproximaciones
         
         if funcion(a)*funcion(x) < 0:
             b = x
@@ -21,4 +23,32 @@ def biseccion(a, b, funcion, iter, tol):
             a =x
 
     print("No se encontró una raíz en el número de iteraciones dadas.")
-    return None
+    return None, aproximaciones, yaproximaciones
+
+
+def secante(a, b, funcion, iter, tol):
+
+    aproximaciones = []
+    yaproximaciones = []
+    
+    for i in range(iter+1):
+
+        if funcion(a) == funcion(b):
+            print("Error: división por 0")
+            return None, [], []
+        
+
+        nuevo_valor = (((a*funcion(b)) - (b*funcion(a)))/(funcion(b)-funcion(a)))
+        aproximaciones.append(nuevo_valor)
+        yaproximaciones.append(funcion(nuevo_valor))
+
+        if funcion(nuevo_valor) == 0 or np.abs(b-nuevo_valor) < tol:
+            print(f"Iteración{i}, raíz aroximada encontrada en: {nuevo_valor}")
+            return nuevo_valor, aproximaciones, yaproximaciones
+
+        a = b
+        b = nuevo_valor
+
+    print("No se halló la raíz")
+    return None, aproximaciones, yaproximaciones
+        
