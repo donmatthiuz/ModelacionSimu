c3df344dd8946cc197d2df6aa5c8980c6ef1e8d4
Gustavo Cruz <cru22779@uvg.edu.gt>
2025-11-06 19:33:50 -0600

    [FEAT] problema 2 final

--- problemas/problema2.py
+++ problemas/problema2.py
@@ -6,12 +6,40 @@ import os
 os.makedirs('../imagenes', exist_ok=True)
 
 N = 100000
-# seed - elegi usar mi carnet
 rng = np.random.Generator(np.random.MT19937(22779))
 u = rng.random(N)
-mean,var = u.mean(),u.var()
-ks = stats.kstest(u,'uniform')
-plt.hist(u,bins=50)
+
+mean = u.mean()
+var = u.var(ddof=0)
+theoretical_mean = 0.5
+theoretical_var = 1.0 / 12.0
+se_mean = np.sqrt(theoretical_var / N)
+z_mean = (mean - theoretical_mean) / se_mean
+p_z_mean = 2 * (1 - stats.norm.cdf(abs(z_mean)))
+
+ks = stats.kstest(u, 'uniform')
+bins = 50
+counts, _ = np.histogram(u, bins=bins, range=(0.0, 1.0))
+expected_counts = np.ones(bins) * (N / bins)
+chisq, p_chisq = stats.chisquare(counts, f_exp=expected_counts)
+autocorr = np.corrcoef(u[:-1], u[1:])[0, 1]
+
+plt.hist(u, bins=bins)
 plt.savefig('../imagenes/problema2_hist_mt.png')
 # plt.show()
-print('MT mean,var,ks.statistic,ks.pvalue', mean, var, ks.statistic, ks.pvalue)
+plt.clf()
+
+plt.scatter(u[:-1], u[1:], s=1)
+plt.xlabel('u_i')
+plt.ylabel('u_{i+1}')
+plt.savefig('../imagenes/problema2_scatter_mt.png')
+# plt.show()
+plt.clf()
+
+print('MT')
+print('N, mean, var', N, mean, var)
+print('theoretical mean,var', theoretical_mean, theoretical_var)
+print('z for mean, p-value', z_mean, p_z_mean)
+print('KS statistic, p-value', ks.statistic, ks.pvalue)
+print('Chi-square, p-value', chisq, p_chisq)
+print('lag-1 autocorrelation', autocorr)
